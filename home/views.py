2667750805b1ae2f7df1735b24f7c68878aa8735
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 18:14:32 +0200

    home view passes quizzes to "home.html"

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Quiz.models import Category,Quiz #,Question,Answer
 
 # Create your views here.
 @login_required
 def home(request):
-	return render(request,"home.html")
+	quizzes = Quiz.objects.all()
+	for quiz in quizzes:
+		if len(quiz.description) > 241:
+			quiz.description = quiz.description[:237] + "..."
+	context = {"quizzes":quizzes}
+	return render(request,"home.html",context)
