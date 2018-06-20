5e8ba0f34f292070d35197185e74ae5ca503cf46
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 18:16:21 +0200

    Added description to Quiz model

--- Quiz/models.py
+++ Quiz/models.py
@@ -13,6 +13,7 @@ class Category(models.Model):
 class Quiz(models.Model):
 	category = models.ForeignKey(Category,on_delete=models.CASCADE)
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	description = models.TextField(max_length=1000)
 	title = models.CharField(max_length=100)
 	photo = models.ImageField(upload_to="quiz/photos")
 	def __str__(self):
