648aebf51131c739958de02f28afc4d4b1620d59
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 14:09:54 +0200

    Added "bootstrapform" to settings.INSTALLED_APPS

--- jLearn/settings.py
+++ jLearn/settings.py
@@ -37,6 +37,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INSTALLED_APPS = [
 	'users',
+	'bootstrapform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
