39e32d4225ebdd30217dcc4124aad076c167ab31
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 15:13:44 +0200

    Created Category model

--- Quiz/models.py
+++ Quiz/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+	def __str__(self):
+		return self.name
+	class Meta:
+		verbose_name = "Categorie"
+
 class Quiz(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
