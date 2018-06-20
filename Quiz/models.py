b0bbb92f79cdf04eaa925564cdd8668e73f4a202
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 14:35:13 +0200

    Added many-to-one relationship between Quiz and User

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Quiz(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
 	photo = models.ImageField(upload_to="quiz/photos")
 	def __str__(self):
