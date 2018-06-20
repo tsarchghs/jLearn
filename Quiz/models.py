8afcb282c39c6c5efe9c24eafb771eac7568cb93
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 15:03:50 +0200

    Created Question/Answer models

--- Quiz/models.py
+++ Quiz/models.py
@@ -9,3 +9,17 @@ class Quiz(models.Model):
 	photo = models.ImageField(upload_to="quiz/photos")
 	def __str__(self):
 		return self.title
+
+class Question(models.Model):
+	quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+	question = models.CharField(max_length=1000)
+	points = models.IntegerField()
+	def __str__(self):
+		return ("#{}#{} - {}".format(self.quiz.id,self.id,self.question))
+
+class Answer(models.Model):
+	question = models.ForeignKey(Question,on_delete=models.CASCADE)
+	answer = models.TextField(max_length=1000)
+	correct = models.CharField(max_length=5,choices=(("0","Not correct"),("1","Correct")))
+	def __str__(self):
+		return ("#{}#{} - {}".format(self.question.id,self.id,self.answer))
