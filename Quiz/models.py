89d263e00a64763fb5d45b70d1948cb2d296a5f0
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 14:31:45 +0200

    Created Quiz model

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+	title = models.CharField(max_length=100)
+	photo = models.ImageField(upload_to="quiz/photos")
+	def __str__(self):
+		return self.title
