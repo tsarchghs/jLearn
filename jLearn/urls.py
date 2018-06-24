c11c2c184a137331a560de17c7efff38415c081a
gjergjk71 <gjergjk71@gmail.com>
2018-06-24 12:25:01 +0200

    Included authentication.urls to urlpatterns

--- jLearn/urls.py
+++ jLearn/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
+    path('auth/', include("authentication.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
