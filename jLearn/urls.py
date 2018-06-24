3fcbe771aad01338c99e383ceb7ffa11cff8d592
gjergjk71 <gjergjk71@gmail.com>
2018-06-24 12:05:57 +0200

    Excluded users.urls from ulpatterns

--- jLearn/urls.py
+++ jLearn/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("users.urls")),
     path('', include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
