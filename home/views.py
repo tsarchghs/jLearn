e208bf6b8dea9c8b367de74420c8185cc2dabe56
gjergjk71 <gjergjk71@gmail.com>
2018-06-24 12:19:30 +0200

    Modified home view for user authentication

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from Quiz.models import Category,Quiz #,Question,Answer
+from django.contrib.auth import authenticate
+from django.contrib.auth.views import login
 
 # Create your views here.
 
@@ -10,4 +12,13 @@ def home(request):
 		if len(quiz.description) > 241:
 			quiz.description = quiz.description[:237] + "..."
 	context = {"quizzes":quizzes}
-	return render(request,"home.html",context)
+	if request.method == "POST":            
+		username=request.POST.get("username")
+		password = request.POST.get("password")                     
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			return login(request)
+		else:
+			return render(request,"home.html",{'invalid': True })
+	else:
+		return render(request,"home.html",context)
