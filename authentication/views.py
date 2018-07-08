4d539a185244781a44e82ae989cfc4fc0248e1b5
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 08:58:14 +0200

    Created custom_login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.views import login
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+def custom_login(request,context):
+	username=request.POST.get("username")
+	password = request.POST.get("password")                     
+	user = authenticate(request, username=username, password=password)
+	if user is not None:
+		return login(request)
+	else:
+		context["invalid"] = True
+		return render(request,"home.html",context)
