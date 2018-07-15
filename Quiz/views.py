cb6befe6da4d21aac574b3b0b79718c6f80398f2
gjergjk71 <gjergjk71@gmail.com>
2018-07-15 15:15:54 +0200

    showQuiz passes question_answers to template

--- Quiz/views.py
+++ Quiz/views.py
@@ -4,5 +4,11 @@ from .models import *
 
 def showQuiz(request,ID):
 	quiz = Quiz.objects.get(pk=ID)
-	context = {"quiz":quiz}
+	question_answers = {}
+	questions = Question.objects.filter(quiz=quiz)
+	answers = Answer.objects.filter(question__in=questions)
+	for question in questions:
+		q_answers = Answer.objects.filter(question=question)
+		question_answers[question] = q_answers
+	context = {"quiz":quiz,"question_answers":question_answers}
 	return render(request,"showQuiz.html",context)
