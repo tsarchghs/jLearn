c8478a5b8516db028668728fa615259ebc13ae2d
gjergjk71 <gjergjk71@gmail.com>
2018-07-15 15:41:42 +0200

    Defined getQuestion_answers function

--- Quiz/views.py
+++ Quiz/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 
-def showQuiz(request,ID):
+def getQuestion_answers(ID):
 	quiz = Quiz.objects.get(pk=ID)
 	question_answers = {}
 	questions = Question.objects.filter(quiz=quiz)
@@ -10,10 +10,15 @@ def showQuiz(request,ID):
 	for question in questions:
 		q_answers = Answer.objects.filter(question=question)
 		question_answers[question] = q_answers
+	return question_answers
+
+def showQuiz(request,ID):
+	quiz = Quiz.objects.get(pk=ID)
+	question_answers = getQuestion_answers(ID)
 	context = {"quiz":quiz,"question_answers":question_answers}
 	return render(request,"showQuiz.html",context)
 
-def submitQuiz(request,ID):
+def submitQuiz	(request,ID):
 	quiz = Quiz.objects.get(pk=ID)
 	if request.method == "POST":
 		pass
