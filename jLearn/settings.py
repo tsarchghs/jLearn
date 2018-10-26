b1794e3d95a0d6fdcc6d9b8b38933d1457cfd245
gjergjk71 <gjergjk71@gmail.com>
2018-10-26 22:16:40 +0200

    Included api.urls to urlpatterns

--- jLearn/settings.py
+++ jLearn/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 	'bootstrapform',
 	'Quiz',
     "authentication",
+    "rest_framework",
+    "api",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +50,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
